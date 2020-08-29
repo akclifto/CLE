5fa852241eab2d1762b84949fd228c2f22eeac4b
akclifto <akclifto@asu.edu>
2020-08-29 14:48:59 -0700

    adding web tutorial for aspnet:

---
+++ WebTut/WebTut.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using WebTut.Website.Models;
+using WebTut.Website.Services;
+
+namespace WebTut.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+
+        //need to put the products somewhere from the json file
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            //get the producuts for the IEnum
+            Products = ProductService.GetProducts();
+        }
+    }
+}

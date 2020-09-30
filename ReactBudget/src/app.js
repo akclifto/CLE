80526205efdb3dd4113483d5b2c3d3bdeb806708
akclifto <akclifto@asu.edu>
2020-09-30 14:21:58 -0700

    set up react-router-dom

--- ReactBudget/src/app.js
+++ ReactBudget/src/app.js
@@ -1,7 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
- ReactDOM.render(<p>Start new app</p>, document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from the dash page
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from the create dummy component!
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from the edit page component!
+    </div>
+
+);
+
+const HelpPage = () => (
+    <div>
+        This is from the help page component!
+    </div>
+
+);
+
+const routes = (
+
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} exact={true}/>
+            <Route path="/edit" component={EditExpensePage} exact={true}/>
+            <Route path="/help" component={HelpPage} />
+        </div>
+        
+        
+
+    </BrowserRouter>
+
+);
+
+ ReactDOM.render(routes, document.getElementById('app'));
  

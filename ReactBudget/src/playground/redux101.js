f7333a310333d0ff1e35b90c239af1104e7d8714
akclifto <akclifto@asu.edu>
2020-10-02 16:34:59 -0700

    redux with dynamic info :

--- ReactBudget/src/playground/redux101.js
+++ ReactBudget/src/playground/redux101.js
@@ -3,49 +3,75 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
 
     switch (action.type) {
+
         case 'INCREMENT' : 
+            const incrementBy = typeof action.incrementBy == 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
+
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy == 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
+
         case 'RESET':
             return {
                 count: 0
             }
+
+        case 'SET':
+            return {
+                count: action.count
+            };
+            
         default :
             return state;
     }
 });
 
-console.log(store.getState());
+//subscribe checks for when the redux state changes for the store var
+const unsub = store.subscribe(() => {
+    console.log(store.getState());
+});
+
 
 // Actions - obj that gets sent to the redux store.  Below are actions.
 
 // We want to incr count, decr count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
-console.log(store.getState());
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 10
 });
-console.log(store.getState());
+
+//to unsuscribe from something to stop watching it, call the var created with the subscribe function.
+// unsub();
+
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
 });
-console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 5
+});
 
 store.dispatch({
     type: 'DECREMENT'
 });
-console.log(store.getState());
 
 // We want to reset count
 store.dispatch({
     type: 'RESET'
 });
-console.log(store.getState());
+
+store.dispatch({
+    type: 'SET',
+    count: 505
+});
 

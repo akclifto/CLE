7599c5d94e7dac813eb4c8fb9dc0f452917ec4b6
akclifto <akclifto@asu.edu>
2020-12-29 16:12:26 -0700

    set up calendar day picker with new libs added

--- ReactBudget/src/components/ExpenseForm.js
+++ ReactBudget/src/components/ExpenseForm.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { sortByAmount } from '../actions/filters';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+// date object for react sucks, using moment.js instead. Documentation is really good for moment
+//const date = new Date();
+// const now = moment();
+// console.log(now.format('MMM DD YYYY'));
 
 export default class ExpenseForm extends React.Component {
 
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
 
     };
 
@@ -17,7 +27,7 @@ export default class ExpenseForm extends React.Component {
 
     setupNoteChange = (e) => {
         const note = e.target.value;
-        this.setState(() => ( { note }));
+        this.setState(() => ({ note }));
     }
 
     onAmountChange = (e) => {
@@ -25,9 +35,17 @@ export default class ExpenseForm extends React.Component {
 
         // using a regular expression here to get a correct value and char input for money to two decimal places
         // search regex101 to learn more about it
-        if(amount.match(/^\d*(\.\d{0,2})?$/)) {
-            this.setState(() => ( { amount }));
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
         }
+    }
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ( { createdAt }));
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ( { calendarFocused: focused }))
     }
 
     render() {
@@ -42,24 +60,35 @@ export default class ExpenseForm extends React.Component {
                         value={this.state.description}
                         onChange={this.onDescriptionChange}
                     />
-                    <input 
+                    <input
                         type="text"
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
                     />
 
-                    <br />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        //number of calendar months visible when selecting a day
+                        numberOfMonths={1}
+                        //inline way to let you select any day in existence 
+                        isOutsideRange={ () => false}
+                    />
                     
+                    <br />
+
                     <textarea
                         placeholder="Add a note for your expense (optional)"
                         value={this.state.note}
                         onChange={this.setupNoteChange}
-                        >
+                    >
                     </textarea>
 
                     <br />
-                    
+
                     <button>Add Expense</button>
                 </form>
             </div>

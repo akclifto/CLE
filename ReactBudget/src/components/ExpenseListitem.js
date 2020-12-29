de9911a77f19ae9fc137b84dc73f431a73435e55
akclifto <akclifto@asu.edu>
2020-12-29 11:14:03 -0700

    add text filter and remove expense, no persist yet

--- ReactBudget/src/components/ExpenseListitem.js
+++ ReactBudget/src/components/ExpenseListitem.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
+
 //Export stateless functional component
 // description, amount, createDate
 
-import React from 'react';
-
-const ExpenseListItem = ({description, amount, createDate}) => (
+const ExpenseListItem = ({dispatch, id, description, amount, createDate}) => (
 
     <div>
         <h3>{description}</h3>
         <p> {amount} - {createDate} </p>
+        <button onClick={() => {
+            dispatch(removeExpense({ id }));
+        }}>Remove</button>
     </div>
 
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);

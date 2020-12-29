7c436e472b211542fdff69da8cded2633a5834a9
akclifto <akclifto@asu.edu>
2020-12-29 11:27:41 -0700

    added filters to sort by date and amount in dashboard

--- ReactBudget/src/components/ExpenseListFilters.js
+++ ReactBudget/src/components/ExpenseListFilters.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortbyDate, sortByAmount } from '../actions/filters';
 
 //make a text filter to search or find things.  
 const ExpenseListFilters = (props) => (
     <div>
-        <input type="text" value={props.filters.text} onChange = {(e) => {
-            props.dispatch(setTextFilter(e.target.value))
-            // console.log(e.target.value)
-        }}/>
+        <input
+            type="text"
+            value={props.filters.text}
+            onChange={(e) => {
+                props.dispatch(setTextFilter(e.target.value))
+                // console.log(e.target.value)
+            }} />
+
+        {/*Below is how to create a controlled input using a text filtering process and drop down selectors.*/}
+        
+        <select
+            value={props.filters.sortBy}
+            onChange={(e) => {
+                if (e.target.value === 'date') {
+                    props.dispatch(sortbyDate())
+                } else if (e.target.value === 'amount') {
+                    props.dispatch(sortByAmount())
+                }
+            }}
+        >
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+        </select>
     </div>
 );
 

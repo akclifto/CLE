55832caa78388afdd65a088df63c3a12341bd40e
akclifto <akclifto@asu.edu>
2020-12-30 11:54:26 -0700

    set up new calendar picker

--- ReactBudget/src/components/ExpenseListFilters.js
+++ ReactBudget/src/components/ExpenseListFilters.js
@@ -1,35 +1,71 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortbyDate, sortByAmount } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortbyDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
+
 
 //make a text filter to search or find things.  
-const ExpenseListFilters = (props) => (
-    <div>
-        <input
-            type="text"
-            value={props.filters.text}
-            onChange={(e) => {
-                props.dispatch(setTextFilter(e.target.value))
-                // console.log(e.target.value)
-            }} />
+class ExpenseListFilters extends React.Component {
 
-        {/*Below is how to create a controlled input using a text filtering process and drop down selectors.*/}
-        
-        <select
-            value={props.filters.sortBy}
-            onChange={(e) => {
-                if (e.target.value === 'date') {
-                    props.dispatch(sortbyDate())
-                } else if (e.target.value === 'amount') {
-                    props.dispatch(sortByAmount())
-                }
-            }}
-        >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-    </div>
-);
+    state = {
+        calendarFocused: null
+    };
+
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({ calendarFocused }));
+    };
+
+    render() {
+        return (
+
+            <div>
+                <input
+                    type="text"
+                    value={this.props.filters.text}
+                    onChange={(e) => {
+                        props.dispatch(setTextFilter(e.target.value))
+                        // console.log(e.target.value)
+                    }} 
+                />
+
+                {/*Below is how to create a controlled input using a text filtering process and drop down selectors.*/}
+
+                <select
+                    value={this.props.filters.sortBy}
+                    onChange={(e) => {
+                        if (e.target.value === 'date') {
+                            this.props.dispatch(sortbyDate());
+                        } else if (e.target.value === 'amount') {
+                            this.props.dispatch(sortByAmount());
+                        }
+                    }}
+                >
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+
+                <DateRangePicker
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={ () => false}
+                />
+
+            </div>
+        );
+    }
+
+}
+
 
 const mapStateToProps = (state) => {
 

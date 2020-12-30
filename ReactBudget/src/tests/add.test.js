df9ec407e80bd3b79f5353210fcd1545578cab75
akclifto <akclifto@asu.edu>
2020-12-30 13:26:56 -0700

    test for expenses

--- ReactBudget/src/tests/add.test.js
+++ ReactBudget/src/tests/add.test.js
@@ -3,7 +3,6 @@ const genGreeting = (name = 'Anon') => `Hello, ${name}!`;
 
 test('should add two numbers', () => {
     const result = add(3,4);
-
     expect(result).toBe(7);
     //  if(result !== 7) {
     //      throw new Error(`Added 4 + 3, result was ${result}. Expected 7`);
@@ -11,9 +10,7 @@ test('should add two numbers', () => {
 });
 
 test('Greeting to John', () => {
-
      const result = genGreeting('John');
-
      expect(result).toBe('Hello, John!');
 
 });

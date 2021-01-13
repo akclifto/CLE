1a8f7be44b6213e3dad728d8ac16dccefce8d372
akclifto <akclifto@asu.edu>
2021-01-13 15:06:34 -0700

    set up webpack for production

--- ReactBudget/src/app.js
+++ ReactBudget/src/app.js
@@ -13,12 +13,13 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configStore();
-console.log(store.getState());
+// console.log('test');
+// console.log(store.getState());
 
-store.dispatch(addExpense( { description: 'Water bill' } ));
-store.dispatch(addExpense({description: 'Gas bill', amount: 30030, createdAt: '01/01/2020'}));
-store.dispatch(addExpense({description: 'Tacos Run', amount: 2000, createdAt: '05/05/2020' }));
-store.dispatch(addExpense({description: 'Rent bill', amount: 100000, createdAt: '06/02/2020'}));
+// store.dispatch(addExpense( { description: 'Water bill' } ));
+// store.dispatch(addExpense({description: 'Gas bill', amount: 30030, createdAt: '01/01/2020'}));
+// store.dispatch(addExpense({description: 'Tacos Run', amount: 2000, createdAt: '05/05/2020' }));
+// store.dispatch(addExpense({description: 'Rent bill', amount: 100000, createdAt: '06/02/2020'}));
 // store.dispatch(setTextFilter('water'));
 
 //shows dynamic nature of rendered components with redux, "water" will initial be displayed, 
@@ -27,11 +28,11 @@ store.dispatch(addExpense({description: 'Rent bill', amount: 100000, createdAt: 
 //     store.dispatch(setTextFilter('bill'));
 // }, 3000);
 
-console.log(store.getState());
+// console.log(store.getState());
 
-const state = store.getState();
-const visExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visExpenses);
+// const state = store.getState();
+// const visExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visExpenses);
 
 // store.dispatch(getVisibleExpenses());
 
